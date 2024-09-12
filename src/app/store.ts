c084551ba6ad308a436a0d67aed0aefc034d1b21
Shilin Timur Sergeevich <roadyleet@gmail.com>
2024-09-12 09:48:02 +0300

    created api

---
+++ src/app/store.ts
@@ -0,0 +1,20 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { api } from "./services/api";
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+export type AppStore = typeof store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = AppStore["dispatch"];
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>;
